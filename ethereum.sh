002ca1e49300e1ec68a89a3f2034168ab423d4fa
Cutepacified <164403716+KineticCiphers@users.noreply.github.com>
2024-05-03 17:12:09 +0300

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-41907dc6-a336-400f-b247-2fb5cabd2b94/"
 
 OUTPUT_DIR="logs"
 
